09d0739afe0c10cd8a1283399543f173b6b6ccbe
GB Hoyt <qrprat77@gmail.com>
2018-01-20 11:13:13 -0500

    Playing with time format.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
-    @render_time = Time.now()
+    @render_time = Time.now().to_formatted_s(:long_ordinal)
   end
 
 end
