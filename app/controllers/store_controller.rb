1997ad9879b9ac4c583166195b73ae857502cc37
GB Hoyt <qrprat77@gmail.com>
2018-01-20 15:56:43 -0500

    Figuring out some formating with currency, adding attribut @units to store_controller.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,6 +2,7 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @render_time = Time.now().to_formatted_s(:long_ordinal)
+    @units = '$' #currency units.
   end
 
 end
