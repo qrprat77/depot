60d89f380925064a798fefb0f2882b948e790419
GB Hoyt <qrprat77@gmail.com>
2018-01-22 22:42:03 -0500

    adding tests and relationships.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_many :line_items
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -6,4 +7,15 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG, or PNG image.'
   }
+  before_destory :ensure_not_referenced_by_any_line_item
+  
+  private
+  
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        error.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
