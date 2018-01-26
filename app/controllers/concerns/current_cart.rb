bc234c0214ecfc1f61d9b4287fe207ba074c6dcc
GB Hoyt <qrprat77@gmail.com>
2018-01-25 22:03:28 -0500

    Making the cart smarter

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -4,6 +4,11 @@ module CurrentCart
   
     def set_cart
       @cart = Cart.find(session[:cart_id])
+      if session[:counter].nil?
+        session[:counter] = 1
+      else
+        session[:counter]+= 1
+      end
     rescue ActiveRecord::RecordNotFound
       @cart = Cart.create
       session[:cart_id] = @cart.id
