3049a58dd6d0dfe04cebf8ab5db8ca4d3a556503
GB Hoyt <qrprat77@gmail.com>
2018-01-25 23:49:06 -0500

    Added migrations to help it cope with numbers in the cart.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126024200) do
+ActiveRecord::Schema.define(version: 20180126030503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
